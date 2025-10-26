dd47c115dd9c92bf9fa74681499828681d070d17
B VENKATASIVAKUMAR <137041447+BVSK203@users.noreply.github.com>
2025-10-26 15:51:50 +0530

    Update configuration values in config.py

--- app/config.py
+++ app/config.py
@@ -1,15 +1,15 @@
 import os
 
 class Config:
-    SECRET_KEY = 'siva'
+    SECRET_KEY = 'your secret key'
     UPLOAD_FOLDER = 'uploads'
     ALLOWED_EXTENSIONS = {'csv'}
 
     # Database Configurations
-    #POSTGRES_URI = "postgresql://siva:Sivakumar203@@localhost:5432/cars_db"
-    POSTGRES_URI = "postgresql+psycopg2://siva:Sivakumar203@@127.0.0.1:5432/cars_db/postgres@PostgreSQL 17*"
+    #POSTGRES_URI = "database url"
+    POSTGRES_URI = "url"
 
-    MONGO_URI = "mongodb://localhost:27017/"
+    MONGO_URI = "mongo database url"
 
     # Ensure upload folder exists
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
